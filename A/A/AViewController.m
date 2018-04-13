d7176b722347edc892fdb5362f7e54a1537ea782
cocoaroger <wuxuanqiang@isoubu.com>
2018-04-13 09:10:10 +0800

    添加podspec

--- A/A/AViewController.m
+++ A/A/AViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "AViewController.h"
+#import <B_Category/CRMediator+B.h>
 
 @interface AViewController ()
 
 @property (strong, nonatomic) UILabel *showLabel;
+@property (strong, nonatomic) UIButton *button;
 
 @end
 
@@ -32,14 +34,54 @@
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-    [_showLabel sizeToFit];
-    _showLabel.center = self.view.center;
+    [self.showLabel sizeToFit];
+    self.showLabel.center = self.view.center;
+    
+    _button.center = CGPointMake(_showLabel.center.x, _showLabel.center.y+50);
+}
+
+- (UILabel *)showLabel {
+    if (_showLabel == nil) {
+        _showLabel = [UILabel new];
+        _showLabel.textColor = [UIColor redColor];
+    }
+    return _showLabel;
 }
 
 - (void)setupViews {
-    _showLabel = [UILabel new];
-    _showLabel.textColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_showLabel];
+    
+    _button = [UIButton new];
+    [_button setTitle:@"进入BViewController" forState:UIControlStateNormal];
+    [_button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [_button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_button sizeToFit];
+    [self.view addSubview:_button];
 }
 
+- (void)buttonAction:(UIButton *)button {
+    UIViewController *bVC = [[CRMediator shareInstance] bViewControllerWithText:@"A进入到B显示文字"];
+    if (self.navigationController) {
+        [self.navigationController pushViewController:bVC animated:YES];
+    } else {
+        [self presentViewController:bVC animated:YES completion:nil];
+    }
+}
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+source 'https://github.com/cr-atomic/CRPrivateRepos.git'
+
+platform :ios, '8.0'
+use_frameworks!
+inhibit_all_warnings!
+
+def pods
+pod 'B', :git => 'https://github.com/cr-atomic/B.git'
+pod 'B_Category', :git => 'https://github.com/cr-atomic/B_Category.git'
+end
+
+target 'A' do
+    pods
+end
